0778c6d167c9d019f07b65ed80153265efa26b7e
Andreano-Yong <220711682@students.uajy.ac.id>
2024-11-28 15:23:46 +0700

    Update Tugas_Modul6_A_Andreano_11682.py

--- Tugas_Modul6_A_Andreano_11682.py
+++ Tugas_Modul6_A_Andreano_11682.py
@@ -7,6 +7,7 @@ from PIL import Image
 # Load the pre-trained model
 # Sesuaikan dengan path model Anda (model terbaik yang di dump dalam format .h5)
 model_path = 'model_mobilenet.h5'
+model = load_model(model_path)
 class_names = ['Matang', 'Mentah']
 
 # Function to preprocess and classify image
